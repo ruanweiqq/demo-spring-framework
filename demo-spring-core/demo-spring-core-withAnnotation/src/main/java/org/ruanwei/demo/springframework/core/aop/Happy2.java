dd7c0ee811c3178e41dfe4edb6be9129d0c4ea22
ruanweiqq <ruanwei@30.18.108.51>
2019-02-20 20:48:28 +0800

    delete stale

--- demo-spring-core/demo-spring-core-withAnnotation/src/main/java/org/ruanwei/demo/springframework/core/aop/Happy2.java
+++
@@ -1,5 +0,0 @@
-package org.ruanwei.demo.springframework.core.aop;
-
-public interface Happy2 {
-    String happy(String msg);
-}
--- demo-spring-core/demo-spring-core-withoutAnnotation/src/main/java/org/ruanwei/demo/springframework/core/aop/Happy.java
+++
@@ -1,5 +0,0 @@
-package org.ruanwei.demo.springframework.core.aop;
-
-public interface Happy {
-    String happy(String msg);
-}
